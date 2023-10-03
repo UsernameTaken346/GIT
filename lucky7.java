f3550688df75f9ded3e7810f7f725041cc230986
M-Lyly <mirzikatajalyly@gmail.com>
2023-10-03 19:02:24 +0300

    Lucky7

---
+++ lucky7.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class lucky7 {
+
+    public static void main(String[] args) {
+        arvoNumerotJaTulosta();
+    }
+
+    public static void arvoNumerotJaTulosta() {
+        Random random = new Random();
+
+        int numero1 = random.nextInt(10) + 1;
+        int numero2 = random.nextInt(10) + 1;
+        int numero3 = random.nextInt(10) + 1;
+
+        System.out.println("Arvotut numerot: " + numero1 + ", " + numero2 + ", " + numero3);
+
+        if (numero1 == 7 || numero2 == 7 || numero3 == 7) {
+            System.out.println("Onneksi olkoon, voitit! Ainakin yksi numero oli 7.");
+        } else {
+            System.out.println("Hävisit! Yksikään numero ei ollut 7.");
+        }
+    }
+}
